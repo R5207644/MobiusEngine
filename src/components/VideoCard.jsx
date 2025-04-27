6065e615eb1de622ad1d2240e3a6bfd836021c6e
Sarvesh Kumar <style.toward@gmail.com>
2025-04-27 21:29:25 +0530

    final

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,7 @@
 import styles from './VideoCard.module.css';
 export default function VideoCard() {
   return <div className={styles.card}>
-    <video src="$" controls></video>
+    <video src="#" controls></video>
     <div className={styles.text}>
      Holly is a senior executive who got over 10 job interviews and an offer she accepted↗
     </div>
